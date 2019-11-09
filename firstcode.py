aa7652f6c538ba5dfca6c1a3f245d8f76951fb27
Oluwasegun Ajikobi <47032225+daevheed13@users.noreply.github.com>
2019-11-09 12:31:20 +0100

    Assignment

--- firstcode.py
+++ firstcode.py
@@ -41,3 +41,15 @@ current_year = 2019
 year_born = current_year - int(age)
 comment = f"hello {name}, you are {age}. You were born in {year_born}"
 print(comment)
+
+year = int(input("Enter a year: "))
+if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
